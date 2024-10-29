8e0e96a8d569a489383e881917710e0ce628bbc7
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2024-10-29 19:06:24 +0000

    Add function to populate display with numbers

--- calculator.js
+++ calculator.js
@@ -2,24 +2,24 @@ let initialInput = 5;
 let operator;
 let followingInput = 10;
 
-let add = function(a, b) {
+let add = function (a, b) {
     return a + b;
 }
 
-let subtract = function(a, b) {
+let subtract = function (a, b) {
     return a - b;
 }
 
-let multiply = function(a, b) {
+let multiply = function (a, b) {
     return a * b;
 }
 
-let divide = function(a, b) {
+let divide = function (a, b) {
     return a / b;
 }
 
-let operate = function(a, operator, b) {
-    if(operator === "+") {
+let operate = function (a, operator, b) {
+    if (operator === "+") {
         return add(a, b);
     } else if (operator === "-") {
         return subtract(a, b);
@@ -30,6 +30,64 @@ let operate = function(a, operator, b) {
     }
 }
 
-let result = operate(initialInput, "/", followingInput);
+const array = [];
 
-alert(result);
+const populateDisplay = function () {
+    const buttons = document.querySelectorAll(".number");
+    const displayDiv = document.querySelector("#display");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            if (button.classList.contains("one")) {
+                array.push(1);
+                displayDiv.textContent = array.reduce((string, value) => string + value, "");
+            }
+            if (button.classList.contains("two")) {
+                array.push(2);
+                displayDiv.textContent = array.reduce((string, value) => string + value, "");
+            }
+            if (button.classList.contains("three")) {
+                array.push(3);
+                displayDiv.textContent = array.reduce((string, value) => string + value, "");
+            }
+            if (button.classList.contains("four")) {
+                array.push(4);
+                displayDiv.textContent = array.reduce((string, value) => string + value, "");
+            }
+            if (button.classList.contains("five")) {
+                array.push(5);
+                displayDiv.textContent = array.reduce((string, value) => string + value, "");
+            }
+            if (button.classList.contains("six")) {
+                array.push(6);
+                displayDiv.textContent = array.reduce((string, value) => string + value, "");
+            }
+            if (button.classList.contains("seven")) {
+                array.push(7);
+                displayDiv.textContent = array.reduce((string, value) => string + value, "");
+            }
+            if (button.classList.contains("eight")) {
+                array.push(8);
+                displayDiv.textContent = array.reduce((string, value) => string + value, "");
+            }
+            if (button.classList.contains("nine")) {
+                array.push(9);
+                displayDiv.textContent = array.reduce((string, value) => string + value, "");
+            }
+            if (button.classList.contains("zero")) {
+                array.push(0);
+                displayDiv.textContent = array.reduce((string, value) => string + value, "");
+            }
+        })
+
+    });
+};
+
+/* document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll(".number");
+    populateDisplay(buttons);
+}) */
+
+populateDisplay();
+
+//alert(result);
